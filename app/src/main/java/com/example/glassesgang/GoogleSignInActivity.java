11561f0db0740f86e787cc95c76838cddd896e00
amreesalmonte <amreesalmonte@gmail.com>
2020-10-25 00:22:06 -0600

    added google signin and firebase auth

--- app/src/main/java/com/example/glassesgang/GoogleSignInActivity.java
+++ app/src/main/java/com/example/glassesgang/GoogleSignInActivity.java
@@ -1,5 +1,6 @@
 package com.example.glassesgang;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -14,85 +15,108 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class GoogleSignInActivity extends AppCompatActivity {
 
-    // user token
     private GoogleSignInClient mGoogleSignInClient;
-    private String TAG;
-    private static int RC_SIGN_IN = 100;
+    private String TAG = "GoogleSignInActivity";
+    private int RC_SIGN_IN = 1;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.sign_in).setOnClickListener((View.OnClickListener) this);
+        setContentView(R.layout.activity_sign_in);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
-
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // sign ins when button clicked
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                signIn();
+            }
+        });
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
-    }
-
-    private void updateUI(GoogleSignInAccount user) {
-        // checks if user is already logged in and redirects to appropriate page
-        if (user != null) {
-            System.out.print("launch main screen");
-        } else {
-            System.out.print("launch sign in screen");
-        }
-    }
-    /*
-    @Override
-    public void onClick(View v) {
-        signIn();
-    }
-    */
     private void signIn() {
+        // prompts the user to choose google account to sign in with
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        // onActivityResult launches automatically after signing in
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed
+                Log.w(TAG, "Google sign in failed", e);
+            }
         }
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+    private void firebaseAuthWithGoogle(String idToken) {
+        // creating credentials
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
 
-            // Signed in successfully, show authenticated UI.
-            updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
-        }
+        // signing in user with credentials
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            updateUI(null);
+                        }
+                    }
+                });
     }
 
+    private void updateUI(FirebaseUser user) {
+        // successful sign in
+        if (user != null) {
+            // redirect to user home page
+            Intent homeIntent = new Intent(this, HomeActivity.class);
+            startActivity(homeIntent);
+            finish();
+        } else {
+            System.out.println("Error Signing In");
+            //display error to user
+        }
+    }
 
 }
--- app/src/main/java/com/example/glassesgang/MainActivity.java
+++ app/src/main/java/com/example/glassesgang/MainActivity.java
@@ -2,15 +2,38 @@ package com.example.glassesgang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        //findViewById(R.id.sign_in).setOnClickListener(this);
+        setContentView(R.layout.activity_main);
+
+        // checks if there's an account signed
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        // redirect to appropriate activity
+        updateUI(account);
+    }
+
+    private void updateUI(GoogleSignInAccount user) {
+        // checks if account signed in
+        if (user != null) {
+            // redirect to user home page
+            Intent homeIntent = new Intent(this, HomeActivity.class);
+            startActivity(homeIntent);
+            finish();
+        } else {
+            // redirect to sign in page
+            Intent signInIntent = new Intent(this, GoogleSignInActivity.class);
+            startActivity(signInIntent);
+            finish();
+        }
     }
 }
