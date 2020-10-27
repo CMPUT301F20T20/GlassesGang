8a7bb49751164fcf71fae7a9d6d818ab70c5533f
JerryD-HybridInfusions <jerry.tdu@hybridinfusions.com>
2020-10-26 19:33:02 -0600

    initial activity and components. material design?

--- app/src/main/java/com/example/glassesgang/MainActivity.java
+++ app/src/main/java/com/example/glassesgang/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.library);
 
         mAuth = FirebaseAuth.getInstance();
 
