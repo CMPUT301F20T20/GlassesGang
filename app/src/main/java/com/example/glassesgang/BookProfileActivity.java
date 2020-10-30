d8cd7cc1aa4d3fbadea4350f784558d251f66549
Cholete99 <union@ualberta.ca>
2020-10-30 13:41:58 -0600

    Added the delete and edit button in book profile. They do nothing yet.

--- app/src/main/java/com/example/glassesgang/BookProfileActivity.java
+++ app/src/main/java/com/example/glassesgang/BookProfileActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentReference;
@@ -14,6 +16,8 @@ public class BookProfileActivity extends AppCompatActivity {
     private TextView authorTextView;
     private TextView isbnTextView;
     private TextView statusTextView;
+    private Button deleteButton;
+    private Button editButton;
     private String author;
     private String title;
     private String isbn;
@@ -35,6 +39,20 @@ public class BookProfileActivity extends AppCompatActivity {
 
         setTextViews();
 
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // implement delete feature
+            }
+        });
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // implement edit feature
+            }
+        });
+
     }
 
     private void findViewsById() {
@@ -42,6 +60,9 @@ public class BookProfileActivity extends AppCompatActivity {
         authorTextView = findViewById(R.id.author_textView);
         isbnTextView = findViewById(R.id.isbn_textView);
         statusTextView = findViewById(R.id.status_textView);
+
+        deleteButton = findViewById(R.id.delete_button);
+        editButton = findViewById(R.id.edit_button);
     }
 
     private void setTextViews() {
