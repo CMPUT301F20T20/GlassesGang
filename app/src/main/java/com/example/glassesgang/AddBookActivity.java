585b34fe5266c8cafac349eded94e6353515ab63
Cholete99 <union@ualberta.ca>
2020-10-30 15:52:37 -0600

    added skeleton code for AddBookActivity

--- app/src/main/java/com/example/glassesgang/AddBookActivity.java
+++ app/src/main/java/com/example/glassesgang/AddBookActivity.java
@@ -3,6 +3,7 @@ package com.example.glassesgang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,6 +19,38 @@ public class AddBookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_edit_book);
+
+        findViewsById();
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // return
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // implement adding
+            }
+        });
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // implement scanning
+            }
+        });
+    }
+
+    private void findViewsById() {
+        backButton = findViewById(R.id.back_button);
+        scanButton = findViewById(R.id.scan_button);
+        saveButton = findViewById(R.id.finish_button);
+        titleEditText = findViewById(R.id.book_title_bar);
+        authorEditText = findViewById(R.id.author_name_bar);
+        isbnEditText = findViewById(R.id.isbn_bar);
     }
 
 }
