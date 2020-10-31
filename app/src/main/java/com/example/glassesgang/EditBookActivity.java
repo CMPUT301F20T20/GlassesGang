346429c041b93e5f84be10bb375f8a69a788d737
William Marshall <wmarshal@ualberta.ca>
2020-10-30 18:23:47 -0600

    Update EditBookActivity.java
    Updated edit book

--- app/src/main/java/com/example/glassesgang/EditBookActivity.java
+++ app/src/main/java/com/example/glassesgang/EditBookActivity.java
@@ -2,6 +2,7 @@ package com.example.glassesgang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,10 @@ public class EditBookActivity extends AppCompatActivity {
     private EditText titleEditText;
     private EditText authorEditText;
     private EditText isbnEditText;
+    private String author;
+    private String title;
+    private String isbn;
+    private String status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,18 @@ public class EditBookActivity extends AppCompatActivity {
         setContentView(R.layout.add_edit_book);
 
         findViewsById();
+
+        Intent intent = getIntent();
+        // implementation of book stats from Cholete
+        MockBook book = (MockBook) intent.getSerializableExtra("Book");
+        author = book.getAuthor();
+        title = book.getTitle();
+        isbn = book.getISBN();
+
+        titleEditText.setText(title);
+        authorEditText.setText(author);
+        isbnEditText.setText(isbn);
+
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,12 +49,14 @@ public class EditBookActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String title = titleEditText.getText().toString();
-                final String author = authorEditText.getText().toString();
-                final String isbn = isbnEditText.getText().toString();
+                title = titleEditText.getText().toString();
+                author = authorEditText.getText().toString();
+                isbn = isbnEditText.getText().toString();
 
                 if (title.length()>0 && author.length()>0 && isbn.length()>0) {
-
+                    // book.setTitle(title)
+                    // book.setAuthor(author)
+                    // book.setISBN(isbn)
                     // somehow add to the system and make sure photos are attached
                     finish();
                 }
