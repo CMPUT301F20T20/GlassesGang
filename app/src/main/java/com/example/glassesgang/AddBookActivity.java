02c3b053be3b6ea999167e170e8d98763c98754a
William Marshall <wmarshal@ualberta.ca>
2020-10-31 00:03:24 -0600

    Update AddBookActivity.java
    implemented adding a new book to the database, still need to figure out adding an image, making a new bid, and scanner. Scanner is taking a lot more time than I thought

--- app/src/main/java/com/example/glassesgang/AddBookActivity.java
+++ app/src/main/java/com/example/glassesgang/AddBookActivity.java
@@ -1,11 +1,27 @@
 package com.example.glassesgang;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.core.OrderBy;
+import com.google.firebase.firestore.core.OrderBy.Direction;
+
+import java.util.HashMap;
+
+import static com.google.firebase.firestore.core.OrderBy.Direction.*;
 
 public class AddBookActivity extends AppCompatActivity {
     private Button backButton;
@@ -14,6 +30,7 @@ public class AddBookActivity extends AppCompatActivity {
     private EditText titleEditText;
     private EditText authorEditText;
     private EditText isbnEditText;
+    private static final String TAG = "AddBookActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +39,15 @@ public class AddBookActivity extends AppCompatActivity {
 
         findViewsById();
 
+        final Intent intent = getIntent();
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish(); // meant to return to the previous activity without adding a book
             }
         });
+
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,11 +57,38 @@ public class AddBookActivity extends AppCompatActivity {
                 final String isbn = isbnEditText.getText().toString();
 
                 if (title.length()>0 && author.length()>0 && isbn.length()>0) {
-                    Book newBook = new Book(title, author, isbn, "???", "AVAILABLE");
-                    // somehow add to the system and make sure photos are attached
+
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                    HashMap<String, Object> newBookInfo = new HashMap<>();
+                    newBookInfo.put("title", title);
+                    newBookInfo.put("author", author);
+                    newBookInfo.put("isbn", isbn);
+                    newBookInfo.put("status", "available");
+
+                    CollectionReference books = db.collection("books");
+
+                    // somehow get a reference to the most recent book to make new bookname
+                    // also figure out how to do images
+                    //
+                    final String newBID = "book100";
+                    books
+                            .document(newBID)
+                            .set(newBookInfo)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "DocumentSnapshot successfully updated!");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error updating document", e);
+                                }
+                            });
                     finish();
                 }
-
             }
         });
 
