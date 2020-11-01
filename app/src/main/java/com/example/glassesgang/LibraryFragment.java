66f6e1ad1e0cd7bbc85ae83b3a1904cc7437f816
Cholete99 <union@ualberta.ca>
2020-11-01 10:09:01 -0700

    can display owner's books now

--- app/src/main/java/com/example/glassesgang/LibraryFragment.java
+++ app/src/main/java/com/example/glassesgang/LibraryFragment.java
@@ -2,6 +2,7 @@ package com.example.glassesgang;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +14,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -24,38 +28,62 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 
 public class LibraryFragment extends Fragment {
+    private ListView bookListView;
+    private ArrayAdapter<Book> bookArrayAdapter;
+    private ArrayList<Book> bookArrayList;
+    private FirebaseFirestore db;
+    final String TAG = "Sample";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =  inflater.inflate(R.layout.library_fragment, container, false);
-        ListView bookListView;
-        final ArrayAdapter<Book> bookArrayAdapter;
-        final ArrayList<Book> bookArrayList;
-        final String TAG = "Sample";
-
+        final View v =  inflater.inflate(R.layout.library_fragment, container, false);
+//        ListView bookListView;
+//        final ArrayAdapter<Book> bookArrayAdapter;
+//        final ArrayList<Book> bookArrayList;
+//
         bookListView = v.findViewById(R.id.library_list_view);
         bookArrayList = new ArrayList<Book>();
         bookArrayAdapter = new CustomBookList(getActivity(), bookArrayList);
         bookListView.setAdapter(bookArrayAdapter);
 
 
-        final FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference booksRef = db.collection("books");
-        booksRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
-                bookArrayList.clear();
+        db = FirebaseFirestore.getInstance();
 
-                for (QueryDocumentSnapshot doc: queryDocumentSnapshots) {
-                    Book book = doc.toObject(Book.class);
-                    book.setBID(doc.getId());
-                    bookArrayList.add(book);
+        // if fragment started from OwnerHomeActivity, get owner catalogue
+        if (getActivity() instanceof OwnerHomeActivity) {
+            DocumentReference ownerRef = db.collection("users").document("someone@ualberta.ca");
+            ownerRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        ArrayList<String> a = (ArrayList<String>) document.get("ownerCatalogue");
+                        updateListView(a, v);
+                        if (document.exists()) {
+                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        } else {
+                            Log.d(TAG, "No such document");
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
                 }
+            });
 
-                bookArrayAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched from the cloud
+        }
+
+        else if (getActivity() instanceof BorrowerHomeActivity) {
+            // get caltalogue of borrower instead
+        }
+
+        else {
+            try {
+                throw new Exception("Libray Fragment can only be started by OwnerHomeActivity or BorrowerHomeActivity");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+        }
 
 
         bookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -106,5 +134,27 @@ public class LibraryFragment extends Fragment {
 //        testListView.setAdapter(allItemsAdapter);
 
         return v;
+    }
+
+    void updateListView(final ArrayList<String> a, View v) {
+        CollectionReference booksRef = db.collection("books");
+
+
+        booksRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                bookArrayList.clear();
+
+                for (QueryDocumentSnapshot doc: queryDocumentSnapshots) {
+                    if (a.contains(doc.getId())) {
+                        Book book = doc.toObject(Book.class);
+                        book.setBID(doc.getId());
+                        bookArrayList.add(book);
+                    }
+                }
+
+                bookArrayAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched from the cloud
+            }
+        });
     }
 }
