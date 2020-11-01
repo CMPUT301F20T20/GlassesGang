33187a89ecbc7a64c8a8c7168eb84fb08f5345af
Cholete99 <union@ualberta.ca>
2020-10-31 23:53:57 -0600

    pass bid in book profile rather than path

--- app/src/main/java/com/example/glassesgang/OwnerHomeActivity.java
+++ app/src/main/java/com/example/glassesgang/OwnerHomeActivity.java
@@ -28,12 +28,10 @@ public class OwnerHomeActivity extends AppCompatActivity {
         // added this, to test OwnerBookProfileActivity -Cholete
         // automatically launches OwnerBookProfileActivity when user goes to OwnerHomeActivity.
 //        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//
 //        DocumentReference docRef = db.collection("books").document("book3");   // sample document reference
-//        Intent bookProfileIntent = new Intent(OwnerHomeActivity.this, OwnerBookProfileActivity.class);
-//        // OwnerBookProfileActivity is passed a path to the  book document
-//        bookProfileIntent.putExtra("path", docRef.getPath());
-//        startActivity(bookProfileIntent);
+        Intent bookProfileIntent = new Intent(OwnerHomeActivity.this, OwnerBookProfileActivity.class);
+        bookProfileIntent.putExtra("bid", "book3");  // pass book id
+        startActivity(bookProfileIntent);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navigationListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
