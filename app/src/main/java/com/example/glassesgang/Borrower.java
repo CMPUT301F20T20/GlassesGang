150b97b900987ca44aa9a26f09b9ed16f1d60808
Chelsea Hong <chelsea4@ualberta.ca>
2020-11-02 13:02:38 -0700

    fixed database calls for owner and updated user object

---
+++ app/src/main/java/com/example/glassesgang/Borrower.java
@@ -0,0 +1,14 @@
+package com.example.glassesgang;
+
+import android.content.Context;
+
+public class Borrower extends User {
+
+    public Borrower(Context context){
+        super(context);
+    }
+
+    public Borrower(){
+        super();
+    }
+}
