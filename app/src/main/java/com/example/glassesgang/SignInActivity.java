d1b402c07e52ef95d44f6f8a1e8e9b919b60cb8f
amreesalmonte <amreesalmonte@gmail.com>
2020-11-17 18:00:57 -0700

    modified front end

--- app/src/main/java/com/example/glassesgang/SignInActivity.java
+++ app/src/main/java/com/example/glassesgang/SignInActivity.java
@@ -1,10 +1,14 @@
 package com.example.glassesgang;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
     private String TAG = "SignInActivity";
@@ -15,8 +19,27 @@ public class SignInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        // init firebase auth
+        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
+    }
+
+    private void updateUI(FirebaseUser user) {
+        // successful sign in
+        if (user != null) {
+            // save email to sharedPreferences
+            String filename = getResources().getString(R.string.email_account);
+            SharedPreferences sharedPref = getSharedPreferences(filename, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("email", user.getEmail());
+            editor.apply();
+            User userobj = new User(user.getEmail());
+            DatabaseManager.createUser(userobj);
+            // redirect to user home page
+            Intent homeIntent = new Intent(this, OwnerHomeActivity.class);
+            startActivity(homeIntent);
+        } else {
+            System.out.println("Error Signing In");
+        }
     }
 }
